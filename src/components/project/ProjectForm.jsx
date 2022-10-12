cdd0cae8ac2f98a0d081bcbd8ba2296077346d03
AlisonLima <alison.juniolima@gmail.com>
2022-10-12 20:25:56 -0300

    db.json criado, options adicionadas

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../form/Input/Input";
 import Select from "../form/Select/Select";
 import SubmitButton from "../form/SubmitButton/SubmitButton";
 import styles from "./ProjectForm.module.css";
 const ProjectForm = ({ btnText }) => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <form className={styles.form}>
       <Input
@@ -20,7 +36,11 @@ const ProjectForm = ({ btnText }) => {
         placeholder="Insira o orçamento total"
       />
 
-      <Select name="category_id" text="selecione a categoria" />
+      <Select
+        name="category_id"
+        text="selecione a categoria"
+        options={categories}
+      />
       <SubmitButton text={btnText} />
     </form>
   );
