dd8065a3673ba1b1c1ff75be842eb01f72724137
AlisonLima <alison.juniolima@gmail.com>
2022-10-20 10:17:58 -0300

    Adicionando novos serviços

--- src/components/pages/Project/Project.jsx
+++ src/components/pages/Project/Project.jsx
@@ -1,15 +1,21 @@
+import { parse, v4 as uuidv4 } from "uuid";
+
 import styles from "./Project.module.css";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Loading from "../../layout/Loading/Loading";
 import Container from "../../layout/Container/Container";
-import ProjectForm from "../../project/ProjectForm";
+import ProjectForm from "../../Project/ProjectForm";
 import Message from "../../layout/Message/Message";
+import ServiceCard from "../../Service/ServiceCard";
+import ServiceForm from "../../Service/ServiceForm";
 
 function Project() {
   let { id } = useParams();
   const [project, setProject] = useState([]);
+  const [service, setService] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
+  const [showServiceForm, setShowServiceForm] = useState(false);
   const [message, setMessage] = useState();
   const [type, setType] = useState();
 
@@ -30,6 +36,7 @@ function Project() {
   }, [id]);
 
   function editPost(project) {
+    setMessage("");
     //budget validation
     if (project.budget < project.cost) {
       setMessage("O orçamento nao pode ser menor do que o custo do projeto!");
@@ -56,8 +63,47 @@ function Project() {
     console.log(project);
   }
 
+  //create Service
+  function createService(project) {
+    setMessage("");
+    //last service
+    const lastService = project.services[project.services.length - 1];
+    lastService.id = uuidv4();
+
+    const lastServiceCost = lastService.cost;
+    const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost);
+
+    //maximo value validation
+    if (newCost > parseFloat(project.budget)) {
+      setMessage("Orçamento ultrapassado, verifique o valor do serviço");
+      setType("error");
+      project.services.pop(); //retira o projeto pois ultrapassou
+      return false;
+    }
+    // add service cost to project total cost
+    project.cost = newCost;
+
+    //update project
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }
+
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
+  }
+
+  function toggleServiceForm() {
+    setShowServiceForm(!showServiceForm);
   }
 
   return (
@@ -96,6 +142,27 @@ function Project() {
                 </div>
               )}
             </div>
+            <div className={styles.service_form_container}>
+              <h2>Adicione um serviço:</h2>
+              <button className={styles.btn} onClick={toggleServiceForm}>
+                {!showServiceForm ? "Adicionar serviço" : "Fechar"}
+              </button>
+            </div>
+            {showServiceForm && (
+              <div className={styles.project_info}>
+                <ServiceForm
+                  handleSubmit={createService}
+                  btnText="Adicionar serviço"
+                  projectData={project}
+                />
+              </div>
+            )}
+            <h2>Serviços</h2>
+            <Container>
+              <div className={styles.project_info}>
+                {service.length > 0 && <ServiceCard />}
+              </div>
+            </Container>
           </Container>
         </div>
       ) : (
